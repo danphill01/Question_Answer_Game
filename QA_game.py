97666894f707fb848291d4fb4215078793802ba9
Daniel Phillips <daniel.j.phillips@outlook.com>
2016-11-26 10:27:35 -0800

    added questions and answers
    Asking for players and names. Set up hardcoded questions and answers

--- QA_game.py
+++ QA_game.py
@@ -15,10 +15,29 @@ points per question change each round
 def print_hline(char='-',width=42):
     print(char*width)
     
-print("Welcome to the game. Here are the categories")
+print("Welcome to the game.")
+players = []
+num_players = input("\nHow many players? ")
+for i in range(1,int(num_players)+1):
+    players.append(input("Player {} name? ".format(i)))
 game_round = 1
 categories = [['cat1','cat2','cat3','cat4','cat5','cat6'],['cat7','cat8','cat9','cat10','cat11','cat12']]
 num_cats = len(categories[game_round-1])
+questions = [
+             {100:"Question1",200:"Question2",300:"Question3",400:"Question4"},
+             {100:"Question5",200:"Question6"}
+            ],[
+             {200:"Question1b",400:"Question2b",800:"Question3b",1600:"Question4b"},
+             {200:"Question5b",400:"Question6b"}
+            ]
+answers = [
+             {100:"Answer1",200:"Answer2",300:"Answer3",400:"Answer4"},
+             {100:"Answer5",200:"Answer6"}
+            ],[
+             {200:"Answer1b",400:"Answer2b",800:"Answer3b",1600:"Answer4b"},
+             {200:"Answer5b",400:"Answer6b"}
+            ]
+print("Here are the categories:")
 for i,cat in enumerate(categories[game_round-1]):
     if i+1==len(categories[game_round-1]):
         print(" {}".format(cat))
@@ -36,5 +55,9 @@ for i in range(1,5+1):
             if j==num_cats:
                 print("  {} ".format(i*100*game_round))
             else: print("  {} |".format(i*100*game_round),end="")
-print_hline()
-    
+    if i<5: print_hline()
+print_hline('=')
+category = int(input("Select a category (1-{}) ".format(num_cats)))
+point_value = int(input("Select a point value (in hundreds) "))*100
+print("The question is:\n  {}".format(questions[game_round-1][category-1][point_value]))
+print("The answer is:\n  {}".format(answers[game_round-1][category-1][point_value]))
